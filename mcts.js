95da7f9e2339fa50b375e1ef77cc29fba33e9c0a
NohTow <ant54600@hotmail.fr>
2018-05-02 17:32:51 +0200

    Codage de la structure de l'arbre + début algo MCTS

--- mcts.js
+++ mcts.js
@@ -50,12 +50,11 @@ Mcts.prototype.notify = function(action){
 
 Mcts.prototype.generate = function(sprites, score){
     var index = getRandomInt(NB_ACTIONS);
-
     var currentSprites = [];
     var sprite;
-    for (var i = 0; i < Game.sprites.length; i++) {
+    for (var i = 0; i < Game.sprites.length; i++) { // Game.sprite = tout les sprites ?
         sprite = sprites[i];
-        if (sprite.visible){
+        if (sprite.visible && sprite.name != 'ship'){
             currentSprites.push(sprite);
             if (sprite.name == 'ship'){
                 if (sprite.bulletCounter <= 0) console.log('canShoot');
@@ -63,6 +62,8 @@ Mcts.prototype.generate = function(sprites, score){
             }
         }
     }
+	
+
 
 
 
@@ -83,3 +84,72 @@ var State = function(direction, speed, closeEnnemies) {
 };
 
 
+//Définition de la structure d'un noeud
+var Noeud = function(){
+	this.init = function(p){
+		this.nbwin = 0;
+		this.nbPlayed = 0;
+		this.pere=p;
+		this.chilD = null;
+		this.chilG = null;
+		this.chilU = null;
+		this.chilS = null;
+		this.spriteList = {};
+	}
+	
+}
+Mcts.prototype.getPlay = function(){
+	for(var i = 0;i<15;i++){
+		runSimu();
+	}
+}
+
+Mcts.prototype.runSimu = function(){
+	var Root = new Noeud{null};
+	root.spriteList = Game.sprites;
+	listCopy = root.spriteList;
+	var index = getRandomInt(NB_ACTIONS);
+	var keyPre = KEY_CODES[index];
+	KEY_STATUS.keyPre = true; // ici j'essaye de lui dire que KEY_STATUS.up ou KEY_STATUS.down etc est pressé => simulation de pression d'une touche
+	thisFrame = Date.now(); // comment définir la frame de base ? a revoir
+    elapsed = thisFrame - lastFrame;
+    lastFrame = thisFrame;
+    delta = elapsed / 30;
+	var tempNode = new Noeud{root};
+	tempNode.spriteList = listCopy; /* plutôt que faire une copie, on peut écrire tempNode.spriteList = root.spriteList mais je sais 
+	pas si du coup modifier tempNode.spriteList modifiera root.spriteList, a voir */
+	 for (i = 0; i < tempNode.length; i++) {
+		if(tempNode.spriteList[i].name == "ship"){temp.node.spriteList[i].preMove(delta);} //maybe useless voir le code de la fonction moove
+        tempNode.spriteList[i].move(delta);
+        if (temp.Node.spriteList[i].reap) { //jsp à quoi sert le .reap mais comme c dans la main loop ça doit être utile je regarderais + tard
+            tempNode.spriteList[i].reap = false;
+            tempNode.spriteList.splice(i, 1);
+            i--;
+        }
+     }
+	 tempNode.nbPlayed++;
+	 switch(keyPre){
+		case 'space':
+		root.chilS = tempNode;
+		break;
+		case 'left' :
+		root.chilG = tempNode;
+		break;
+		case 'up' :
+		root.chilU = tempNode;
+		break;
+		case 'right' :
+		root.chilD = tempNode;
+		break;
+		default:
+	 }
+	
+	
+	
+	}
+//32: 'space',
+    37: 'left',
+    38: 'up',
+    39: 'right',
+
+}
